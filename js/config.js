d9641064786152f407df48345cbeab1fb751e615
leenus06 <146060227+leenus06@users.noreply.github.com>
2025-09-08 12:07:18 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_X0VR5lJom', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '67c3b2bgtvtofc8432e2oafocg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_lGnM0bvv9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38f8einkj1m20j00laqa8kmd97', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://b6y437ysae.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
